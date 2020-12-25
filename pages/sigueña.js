dcd157a53353a998f43bfa939aa2ebd35f1e356f
Nahuel <nahuefer105@outlook.com>
2020-12-25 16:07:11 -0300

    Problem solved: I could not read JSON files

--- pages/sigueña.js
+++ pages/sigueña.js
@@ -9,26 +9,4 @@ function ReadJSONFile(jsonPath)
 
 }
 
-var d = new Date();
-let JSON =
-[
-    {
-        "name": "",
-        "photoPath": name + "IMG",
-        "inscriptionDay": 14,
-        "inscriptionMonth": 3,
-        "inscriptionYear": 2020
-    },
-    {
-        "name": "Nahuel",
-        "photoPath": name + "IMG",
-        "inscriptionDay": 14,
-        "inscriptionMonth": 3,
-        "inscriptionYear": 2020
-    }
-]
-
-    //console.log(JSON[0].name);
-    //console.log(Object.keys(JSON).length);
-JSON[0].name = "tuvieja";  
 document.getElementById("Texto").innerHTML = readMe;
