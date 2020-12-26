27dc33ef0086fa3bfd6a748c002ea50b01a9eb28
Nahuel <nahuefer105@outlook.com>
2020-12-26 12:02:31 -0300

    Prepare things to read and write files

--- pages/sigueña.js
+++ pages/sigueña.js
@@ -1,12 +1,41 @@
 var fs = require('fs');
 
 var rawdata = fs.readFileSync('socios.json');
-var socios = JSON.parse(rawdata);
-console.log(socios);
+var socios = JSON.parse(rawdata);;
+
+var nombresito = "OsitoSimon";
+
+var sociosElement = 
+{
+    "names": nombresito,
+    "lastName": "",
+    "direction": "",
+    "dni": "",
+    "cellphone": "",
+    "alternativeCellphone": "",
+    "email": "",
+    "observations": ""
+}
+var sociosArray =
+[
+    
+]
+
+for(var i = 0; i < 10; i++)
+{
+    sociosArray[i] = sociosElement;
+}
+var data = JSON.stringify(sociosArray, null, 2);
+fs.writeFileSync('socios.json', data);
+console.log(data);
+console.log(socios[5]);
+
+console.log(Object.keys(sociosArray).length);
+
 
 function ReadJSONFile(jsonPath)
 {
 
 }
 
-document.getElementById("Texto").innerHTML = readMe;
+//document.getElementById("Texto").innerHTML = readMe;
