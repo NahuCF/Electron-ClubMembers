1bb8655c68eb7acb5d3c7a6637c27eadd5201de5
Nahuel <nahuefer105@outlook.com>
2020-12-29 11:44:40 -0300

    Functions to Read and Write the JSON file

--- pages/sigueña.js
+++ pages/sigueña.js
@@ -1,13 +1,17 @@
 var fs = require('fs');
 
-var rawdata = fs.readFileSync('socios.json');
-var socios = JSON.parse(rawdata);;
-
-var nombresito = "OsitoSimon";
+var varNames;
+var varLastName;
+var varDirection;
+var varDni;
+var varCellphone;
+var varAlternativeCellPhone;
+var varEmail;
+var varObservations;
 
 var sociosElement = 
 {
-    "names": nombresito,
+    "names": "",
     "lastName": "",
     "direction": "",
     "dni": "",
@@ -21,21 +25,52 @@ var sociosArray =
     
 ]
 
-for(var i = 0; i < 10; i++)
+
+document.getElementById("Send-Data").onclick = SendDataToVariables;
+
+function SendDataToVariables()
 {
-    sociosArray[i] = sociosElement;
-}
-var data = JSON.stringify(sociosArray, null, 2);
-fs.writeFileSync('socios.json', data);
-console.log(data);
-console.log(socios[5]);
-
-console.log(Object.keys(sociosArray).length);
-
-
-function ReadJSONFile(jsonPath)
-{
-
+    varNames = document.getElementById("Texto").textContent;
+    CopyDataToArray();
 }
 
-//document.getElementById("Texto").innerHTML = readMe;
+function CopyDataToArray()
+{
+    sociosElement.names = varNames;
+    sociosElement.lastName = varLastName;
+    sociosElement.direction = varDirection;
+    sociosElement.dni = varDni;
+    sociosElement.cellphone = varCellphone;
+    sociosElement.alternativeCellphone = varAlternativeCellPhone;
+    sociosElement.email = varEmail;
+    sociosElement.observations = varObservations;
+    
+    WriteJSONFile();
+}
+function WriteJSONFile()
+{  
+    var rawdata = fs.readFileSync('socios.json');
+
+    sociosArray[0] = sociosElement;
+
+    if(rawdata.byteLength == 0)
+    {
+        sociosArray.splice(0, sociosArray.length);
+
+        sociosArray[0] = sociosElement; 
+    }
+    else if(rawdata.byteLength > 0)
+    {
+        sociosArray.splice(0, sociosArray.length);
+        let socios = JSON.parse(rawdata)
+
+        for(var i = 0; i < Object.keys(socios).length; i++)
+        {
+            sociosArray[i] = socios[i];
+        }
+        sociosArray[sociosArray.length] = sociosElement;
+    }
+    
+    let data = JSON.stringify(sociosArray, null, 2);
+    fs.writeFileSync('socios.json', data);
+}
